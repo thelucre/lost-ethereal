583f2b4b0f35c03b9ed1ef4b8d12a0396f0fab4b
Eric Howard <howard.eric.m@gmail.com>
2016-04-19 21:04:17 -0700

    sprite settings and logo scene

---
+++ Assets/Scripts/LogoScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("StartGame", 4f);
+	}
+	
+	void StartGame() {
+		SceneManager.LoadScene("Game");
+	}
+}

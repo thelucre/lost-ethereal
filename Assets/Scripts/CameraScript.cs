fe03cc88f8866f3ed20def1a2c9a0b1d2138c0cb
Eric Howard <howard.eric.m@gmail.com>
2016-04-19 19:27:08 -0700

    pixel perfect test

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Target) {
+			transform.position = new Vector3(
+				Mathf.Floor(Target.position.x/64)*64+32,
+				Mathf.Floor(Target.position.y/64)*64+32,
+				-10
+			);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float Speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movex = Input.GetAxisRaw("Horizontal");
+		float movey = Input.GetAxisRaw("Vertical");
+
+		transform.Translate(movex*Speed*Time.deltaTime, movey*Speed*Time.deltaTime, 0);
+	}
+}

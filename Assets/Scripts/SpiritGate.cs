627720a06572652fa1aa59c5ecc24d5e2bca6863
Eric Howard <howard.eric.m@gmail.com>
2016-04-23 13:27:50 -0700

    spirit gate added

---
+++ Assets/Scripts/SpiritGate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritGate : ActivateBase {
+
+	public AudioClip GateOpenSound;
+
+	Collider2D collider;
+	SpriteRenderer sr;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		collider = gameObject.GetComponent<Collider2D>();
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		audio = gameObject.GetComponent<AudioSource>();
+
+		collider.enabled = false;
+		sr.enabled = false;
+	}
+	
+	public override void Activate() {
+		Debug.Log("activating spirit gate");
+		collider.enabled = true;
+		sr.enabled = true;
+		audio.Play();
+	}
+
+	public override void Deactivate() {
+		collider.enabled = false;
+		sr.enabled = false;
+		audio.clip = GateOpenSound;
+		audio.Play();
+	}
+}
---
+++ Assets/Scripts/SpiritgateTriggerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiritgateTriggerScript : ActivateBase {
+
+	public GameObject SpiritGate;
+	public string State = "open";
+
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if(other.gameObject.tag != "player") return;
+
+		if(State == "open") { 
+			Activate(); 
+			State = "closed";
+		} else if (State == "closed" && Settings.wand) {
+			Deactivate();
+			Destroy(gameObject);
+		}
+	}
+
+	public override void Activate() {
+		SpiritGate.SendMessage("Activate");
+		Debug.Log(SpiritGate);
+	}
+
+	public override void Deactivate() {
+		SpiritGate.SendMessage("Deactivate");
+	}
+}

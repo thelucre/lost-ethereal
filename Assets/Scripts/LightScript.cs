a5609005f7b3ee53950da2b2ecb46e8f159b5212
Eric Howard <howard.eric.m@gmail.com>
2016-05-01 14:55:29 -0700

    work on desert boss

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour {
+
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		Deactivate();
+	}
+
+	public void Activate() {
+		sr.enabled = true;
+	}
+
+	public void Deactivate() {
+		sr.enabled = false;
+	}
+
+}
---
+++ Assets/Scripts/LightSwitchScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitchScript : ActivateBase {
+
+	public LightScript Light;
+	public Sprite ActiveSprite;
+	public LightSwitchScript[] others;
+
+	Sprite InactiveSprite;
+	SpriteRenderer sr;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = gameObject.GetComponent<AudioSource>();
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		InactiveSprite = sr.sprite;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag != "player") return;
+
+		audio.Play();
+
+		Active = !Active;
+		if(Active) Activate(); else Deactivate();
+	}
+	
+	public override void Activate() {
+		Active = true; 
+		sr.sprite = ActiveSprite;
+		Light.Activate();
+
+		foreach(LightSwitchScript ls in others) ls.Deactivate();
+	}
+
+	public override void Deactivate() {
+		Active = false; 
+		sr.sprite = InactiveSprite;
+		Light.Deactivate();
+	}
+}

d3f299013e71de283bb019e6cde2f6d776928d90
Eric Howard <howard.eric.m@gmail.com>
2016-05-01 17:53:53 -0700

    lights moving

--- Assets/Scripts/LightMoveScript.cs
+++ Assets/Scripts/LightMoveScript.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LightMoveScript : MonoBehaviour {
+public class LightMoveScript : ActivateBase {
+
+	public Sprite ActiveSprite;
+	public Vector2 Direction;
+
+	Sprite InactiveSprite;
+	SpriteRenderer sr;
+	AudioSource audio;
+	LightScript[] lights;
 
 	// Use this for initialization
 	void Start () {
-	
+		audio = gameObject.GetComponent<AudioSource>();
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		lights = GameObject.FindObjectsOfType<LightScript>();
+		InactiveSprite = sr.sprite;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void Update() {
+		if(Active) MoveActiveLight();
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag != "player") return;
+
+		audio.Play();
+
+		Active = true;
+		Activate();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.tag != "player") return;
+
+		audio.Play();
+
+		Active = false;
+		Deactivate();
+	}
+
+	public override void Activate() {
+		Active = true; 
+		sr.sprite = ActiveSprite;
+
+	}
+
+	public override void Deactivate() {
+		Active = false; 
+		sr.sprite = InactiveSprite;
+	}
+
+	void MoveActiveLight() {
+		foreach(LightScript l in lights) {
+			if(l.Active) l.Move( Direction );
+		}
 	}
 }
--- Assets/Scripts/LightScript.cs
+++ Assets/Scripts/LightScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class LightScript : MonoBehaviour {
 
 	SpriteRenderer sr;
+	float Speed = 6f;
+	public bool Active = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +15,18 @@ public class LightScript : MonoBehaviour {
 
 	public void Activate() {
 		sr.enabled = true;
+		Active = true;
 	}
 
 	public void Deactivate() {
 		sr.enabled = false;
+		Active = false;
+	}
+
+	public void Move(Vector2 direction) {
+		Vector3 move = direction;
+		move *= (Time.deltaTime * Speed);
+		transform.Translate(move);
 	}
 
 }
